862f9418734750c6f1abd4d0b7f1c8748785c8ce
jia <jbia@itu.dk>
2021-12-13 20:32:16 +0100

    Add test for ImageRepository

--- Server.Repositories.Tests/ImageRepositoriesTest.cs
+++ Server.Repositories.Tests/ImageRepositoriesTest.cs
@@ -1,17 +1,22 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using Azure.Identity;
 using Azure.Storage;
 using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using SETraining.Server.Repositories;
 using SETraining.Shared;
 using Xunit;
 
 namespace Server.Repositories.Tests;
 
-public class ImageRepositoriesTest
+public class ImageRepositoriesTest : IDisposable
 {
-    private BlobContainerClient _client;
+    private BlobServiceClient _serviceClient;
+    private BlobContainerClient _containerClient;
     private ImageRepository _repository;
     private string _mockUserName;
     private string _mockContainerName;
@@ -20,29 +25,48 @@ public class ImageRepositoriesTest
     {
         //Account name
         _mockUserName = "devstoreaccount1";
-        _mockContainerName = "devstoretest";
+        _mockContainerName = "images";
         
-        // With account name and key
-        _client = new BlobContainerClient(
-            "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1",
-            _mockContainerName
-        );
-
-        _repository = new ImageRepository(_client);
+        _serviceClient = new BlobServiceClient(
+            "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;");
+        
+        _containerClient = _serviceClient.CreateBlobContainer("imagetest");
+        
+        _repository = new ImageRepository(_containerClient);
     }
     
     
 
     [Fact]
-    public async Task Sucessful_Create_returns_url()
+    public async Task Create_returns_uri()
     {
         //Arrange
         var ExpectedResult = Status.Created;
+        
         //Act
-        var result = await _repository.CreateImageAsync("tester.jpg", "jpeg", Stream.Null);
+        var result = await _repository.CreateImageAsync("tester.jpg", "jpeg",  new MemoryStream());
 
         //Assert
-        Assert.Equal(result.status, ExpectedResult);
         Assert.NotNull(result.uri);
+    }
+    
+    [Fact]
+    public async Task CreateImageAsync_returns_status_Created()
+    {
+        //Arrange
+        var ExpectedResult = Status.Created;
+        
+        //Act
+        var result = await _repository.CreateImageAsync("tester.jpg", "jpeg",  new MemoryStream());
+
+        //Assert
+        Assert.Equal(result.status, ExpectedResult); 
+    }
+    
+    //TODO: Lav test med data fra stream
+
+    public void Dispose()
+    {
+         _containerClient.Delete();
     }
 }
