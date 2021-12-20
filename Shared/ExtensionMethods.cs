2957a6851d2ff5094d1fe30dfd56596ff93cf944
Joachim Alexander Kofoed <jkof@itu.dk>
2021-12-20 22:56:25 +0100

    - Add new test project Server.Tests - Add missing action result to ToActionResult() - Add tests for ToActionResult()

--- Shared/ExtensionMethods.cs
+++ Shared/ExtensionMethods.cs
@@ -4,4 +4,5 @@ public static class ExtensionMethods
 {
     public static bool IsNullOrEmpty<T>(this ICollection<T> col) => (col == null || col.Count == 0);
     public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> self) => self?.Select((item, index) => (item, index)) ?? new List<(T, int)>();
+    //TODO: remove this method???
 }
